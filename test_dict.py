2128920f65fbf13aee9f748d12499a0d52e09a98
AurusDev <103201978+AurusDev@users.noreply.github.com>
2026-03-03 11:38:21 -0300

    chore: remove arquivos de debug/teste e organiza scripts

--- test_dict.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-from supabase import create_client
-
-load_dotenv()
-c = create_client(os.environ['SUPABASE_URL'], os.environ['SUPABASE_KEY'])
-res = c.table('systems').select('*').limit(1).execute()
-print(res.data)
